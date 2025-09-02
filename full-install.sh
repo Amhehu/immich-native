ed4470f66837e3f9597e7b8118f488bba1885c44
Silvio Tristram <54313650+Amhehu@users.noreply.github.com>
2025-09-02 11:37:57 +0200

    changes for Debian 13, but still not working

--- full-install.sh
+++ full-install.sh
@@ -4,20 +4,20 @@
 apt update
 apt -y upgrade
 
-apt install -y curl git gpg lsb-release postgresql-common
+apt install -y curl git gpg lsb-release postgresql-common cmake
 apt install -y --no-install-recommends python3-venv python3-dev uuid-runtime autoconf build-essential unzip jq perl libnet-ssleay-perl libio-socket-ssl-perl libcapture-tiny-perl libfile-which-perl libfile-chdir-perl libpkgconfig-perl libffi-checklib-perl libtest-warnings-perl libtest-fatal-perl libtest-needs-perl libtest2-suite-perl libsort-versions-perl libpath-tiny-perl libtry-tiny-perl libterm-table-perl libany-uri-escape-perl libmojolicious-perl libfile-slurper-perl liblcms2-2 wget
 
 cd /tmp/
 
 # nodejs
-curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
+curl -fsSL https://deb.nodesource.com/setup_24.x -o nodesource_setup.sh
 sudo -E bash nodesource_setup.sh
 apt install -y nodejs
 
 # redis
 curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/redis.list
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb bookworm main" | tee /etc/apt/sources.list.d/redis.list
 
 apt update
 apt install -y redis-server
